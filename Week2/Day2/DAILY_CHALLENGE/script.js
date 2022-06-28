fa81c87878e11de6223c3a20cfdc376a42f734ae
anaelle daoudi <anaelledaoudi@gmail.com>
2022-06-28 14:40:33 +0300

    daily challenge week day 2 with correct answer

--- Week2/Day2/DAILY_CHALLENGE/script.js
+++ Week2/Day2/DAILY_CHALLENGE/script.js
@@ -1,12 +1,15 @@
 let sentance="The movie is not that bad, I like it";
-notIndex=sentance.indexOf("not");
-badIndex=sentance.indexOf("bad");
+let newSentance;
+wordNot=sentance.indexOf("not");
+//console.log(wordNot);
+wordBad=sentance.indexOf("bad");
+//console.log(wordBad);
 
-if (notIndex<badIndex) {
-	sentance.substring(0,notIndex);
-	sentance[notIndex]="good";
-	sentance[notIndex+4]=sentance.substring(badIndex+3,sentance.length);
-	console.log(sentance);
+if (wordNot<wordBad) {
+	newSantance=sentance.substring(0,wordNot);
+	newSantance+="good";
+	newSantance+=sentance.substring(wordBad+3,sentance.length);
+    console.log(newSantance);
 }
 else{
 	console.log(sentance)
