fd7e681df9c4c29337bbfa0d9874cc37baca136a
anaelle daoudi <anaelledaoudi@gmail.com>
2022-06-30 10:23:53 +0300

    correct errors

--- Week2/Day4/DAILY_CHALLENGE/script.js
+++ Week2/Day4/DAILY_CHALLENGE/script.js
@@ -1,8 +1,8 @@
 let sentance=prompt("hi enter severalc words");
 let sentanceArr=sentance.split(" ");
 function longestWord(arr){
-	let bigWord=0;
-	for (let i=0; i<arr.length; i++){
+	let bigWord=arr[0].length;
+	for (let i=1; i<sentanceArr.length; i++){
 	if(arr[i].length>bigWord){
 		bigWord=arr[i].length;
 	}
@@ -10,7 +10,7 @@ function longestWord(arr){
   return bigWord;
 }
 function spaces(word,len){
-if(word.length=len){
+if(word.length==len){
    return "";
   }
   return(" ".repeat(len-word.length))
@@ -18,8 +18,8 @@ if(word.length=len){
 function stars(){
 	let bigWord=longestWord(sentanceArr);
 	console.log("*".repeat(bigWord+4));
-	for(let x in sentance){
-		console.log("*"+sentance[x]+spaces(sentance[x],bigWord)+"*")
+	for(let x in sentanceArr){
+		console.log("* "+sentanceArr[x]+spaces(sentance[x],bigWord)+" *")
 	}
 	console.log("*".repeat(bigWord+4));
 }
