f58c784fc1e476cf9cdf8f456ed8f13f8c79c571
anaelle daoudi <anaelledaoudi@gmail.com>
2022-06-30 10:17:17 +0300

    daily challenge

--- Week2/Day4/DAILY_CHALLENGE/script.js
+++ Week2/Day4/DAILY_CHALLENGE/script.js
@@ -1,5 +1,27 @@
 let sentance=prompt("hi enter severalc words");
 let sentanceArr=sentance.split(" ");
-for (let i=0; i<sentanceArr.length; i++){
-	console.log(sentanceArr[i]);
+function longestWord(arr){
+	let bigWord=0;
+	for (let i=0; i<arr.length; i++){
+	if(arr[i].length>bigWord){
+		bigWord=arr[i].length;
+	}
+  }
+  return bigWord;
 }
+function spaces(word,len){
+if(word.length=len){
+   return "";
+  }
+  return(" ".repeat(len-word.length))
+}
+function stars(){
+	let bigWord=longestWord(sentanceArr);
+	console.log("*".repeat(bigWord+4));
+	for(let x in sentance){
+		console.log("*"+sentance[x]+spaces(sentance[x],bigWord)+"*")
+	}
+	console.log("*".repeat(bigWord+4));
+}
+stars();
+
