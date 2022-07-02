aaf2ee3abf86b195461c9d751c06657b8cfc3d83
anaelle daoudi <anaelledaoudi@gmail.com>
2022-07-02 22:58:20 +0300

    daily challenge done

--- Week2/Day5/DAILY_CHALLENGE/script.js
+++ Week2/Day5/DAILY_CHALLENGE/script.js
@@ -1,17 +1,17 @@
-function lessBottle(bottle,num){
-   console.log(`we have now ${num-bottle} bottles`)
-}
 function printSong(num){
 	let bottle=2;
-	do{
-		console log("Take 1 down, pass it around");
-		console.log(`we have now ${--num} bottle`);
-	}
-	while(num!=0){
+	console.log("Take 1 down, pass it around");
+	console.log(`we have now ${num-=1} bottles`);
+	while(num>=0){
 		console.log(`Take ${bottle} down, pass them around`);
-		lessBottle(bottle,num);
+		 if(bottle>=num){
+		console.log(`we have now 0 bottle`);
+		num=-1;
+	    }
+	    else{
+		console.log(`we have now ${num-=bottle} bottles`);
 		bottle++;
-		num--;
+	   }
 	}
 }
 
