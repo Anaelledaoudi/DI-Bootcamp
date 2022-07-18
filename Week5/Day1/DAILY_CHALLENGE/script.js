3a03f0cab23ea386c6323d627f82b2508b238f89
anaelle daoudi <anaelledaoudi@gmail.com>
2022-07-18 10:00:26 +0300

    correct errors

--- Week5/Day1/DAILY_CHALLENGE/script.js
+++ Week5/Day1/DAILY_CHALLENGE/script.js
@@ -1,3 +1,17 @@
-const formInfo=document.getElementById("form");
-console.log(formInfo);
-const toJson=JSON.stringify(formInfo);
+
+let form=document.getElementById("form");
+form.addEventListener("submit",retrieveData);
+function retrieveData(event){
+event.preventDefault();
+const name=document.getElementById("name").value;
+const lastName=document.getElementById("lastName").value;
+console.log(name,lastName);
+const obj={
+	userName:name,
+	userLastName:lastName	
+}
+ const toJson=JSON.stringify(obj);
+ let p=document	.createElement("p");
+ p.textContent=toJson;
+ document.body.appendChild(p);
+}
